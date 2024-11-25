0949e2620add61baeb12f692020a2ef87def9060
Shrimp_RedCake <nicolebatistabonbon@gmail.com>
2024-11-25 10:03:03 -0300

    Add files via upload

---
+++ crs/polygonpalls.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('shhh');
+const text = document.getElementById('mytext')
+
+button.addEventListener('click', function(){
+    text.textContent ='botaaaaaaaaao';
+    text.style.color = 'blue'
+})
